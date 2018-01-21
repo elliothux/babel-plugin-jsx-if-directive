773409dcd334f1d6648e781b34e20ba51ff77a10
胡青杨 <i@do.codes>
2018-01-21 19:28:12 +0800

    Supporting "else" expression

--- index.js
+++ index.js
@@ -7,25 +7,53 @@ module.exports = function ({types: t}) {
 
         path.traverse({ JSXElement: JSXElementVisitor });
 
-        const ifBinding = getAndRemoveIfBinding(path.node.openingElement);
+        const ifBinding = getAttrAST(path.node.openingElement, 'if');
         if (ifBinding) {
+            const nextNode = findNextNode(path, path.parent.children, path.key);
+            let alertnateAst = getAndRemoveAlertnateAst(nextNode);
+            if (alertnateAst.type !== 'NullLiteral')
+                path.parent.children = path.parent.children.filter(node => node !== nextNode);
+
             path.replaceWith(
                 t.conditionalExpression(
                     ifBinding.value.expression,
                     path.node,
-                    t.nullLiteral()
+                    alertnateAst
                 )
-            )
+            );
         }
 
-        function getAndRemoveIfBinding(openingElement) {
-            if (openingElement.type !== 'JSXOpeningElement') return;
+        function getAndRemoveAlertnateAst(node) {
+            const elseBinding = getAttrAST(node.openingElement, 'else');
+            if (elseBinding) return node;
+
+            // const elseIfBinding = getAttrAST(node.openingElement, 'elif', false);
+            // if (elseIfBinding) {
+            //     console.log(elseIfBinding);
+            //     return node;
+            // }
+
+            return t.nullLiteral();
+        }
+
+        function findNextNode(path, sibling, key) {
+            if (!sibling) return null;
+            const nextPath = sibling[key + 1];
+            if (!nextPath) return null;
+            if (nextPath.type === 'JSXText' && !nextPath.value.trim())
+                return findNextNode(nextPath, sibling, key+1);
+            return nextPath.type === 'JSXElement' ? nextPath : null;
+        }
+
+        function getAttrAST(openingElement, attrName, willRemove=true) {
+            if (openingElement.type !== 'JSXOpeningElement') return null;
             const index = openingElement.attributes.findIndex(attr => attr.name && attr.name.name === attrName);
-            if (index >= 0) {
-                const ifBinding = openingElement.attributes[index];
-                openingElement.attributes = openingElement.attributes.filter(attr => attr !== ifBinding);
-                return ifBinding;
-            }
+            if (index < 0) return null;
+
+            const attrBinding = openingElement.attributes[index];
+            if (willRemove)
+                openingElement.attributes = openingElement.attributes.filter(attr => attr !== attrBinding);
+            return attrBinding;
         }
     }
 
