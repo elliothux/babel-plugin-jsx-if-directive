e0852862e1ef0bcdebb2dfcabc77c127731397c1
HuQingyang <i@do.codes>
2018-12-17 11:29:59 +0800

    feat: getOptions

--- index.js
+++ index.js
@@ -1,82 +1,74 @@
 
 const {
-    removeNode,
-    findNextNode,
-    transformIfBinding,
-    transformElseBinding,
-    transformElseIfBindings,
-    getAttrASTAndIndexByName
+  removeNode,
+  findNextNode,
+  transformIfBinding,
+  transformElseBinding,
+  transformElseIfBindings,
+  getAttrASTAndIndexByName
 } = require('./utils');
 
+const defaultOpts = {
+  ifAttrName: 'if',
+  elseAttrName: 'else',
+  elseIfAttrName: 'elseIf'
+};
 
 module.exports = function () {
-    function JSXElementVisitor(path) {
-        const {
-            ifAttrName,
-            elseAttrName,
-            elseIfAttrName
-        } = getAttrNamesFromOption();
+  function JSXElementVisitor(path, { opts = defaultOpts }) {
+    const {
+      ifAttrName,
+      elseAttrName,
+      elseIfAttrName
+    } = opts;
 
-        const ifBinding = getAttrASTAndIndexByName(path.node, ifAttrName);
-        if (!ifBinding) return;
+    const ifBinding = getAttrASTAndIndexByName(path.node, ifAttrName);
+    if (!ifBinding) return;
 
-        let {
-            parent: { children: siblings },
-            key: index
-        } = path;
+    let {
+      parent: { children: siblings },
+      key: index
+    } = path;
 
-        let nextNode = findNextNode(path, siblings, index);
-        if (!nextNode) return transformIfBinding(path, ifBinding);
+    let nextNode = findNextNode(path, siblings, index);
+    if (!nextNode) return transformIfBinding(path, ifBinding);
 
-        let elseBinding = getAttrASTAndIndexByName(nextNode, elseAttrName);
-        const elseIfBindings = [];
+    let elseBinding = getAttrASTAndIndexByName(nextNode, elseAttrName);
+    const elseIfBindings = [];
 
-        if (!elseBinding) {
-            let elseIfBinding = getAttrASTAndIndexByName(nextNode, elseIfAttrName);
-            while (elseIfBinding) {
-                elseIfBindings.push(elseIfBinding);
-                index += 1;
-                nextNode = findNextNode(path, siblings, index);
-                elseIfBinding = nextNode ?
-                    getAttrASTAndIndexByName(nextNode, elseIfAttrName) :
-                    null;
-            }
-            if (nextNode) {
-                elseBinding = getAttrASTAndIndexByName(nextNode, elseAttrName);
-            }
-        }
-
-        if (elseIfBindings.length > 0) {
-            transformElseIfBindings(path, ifBinding, elseIfBindings, elseBinding);
-
-            elseIfBindings.forEach((binding) => {
-                removeNode(siblings, binding.node);
-            });
-            if (elseBinding) {
-                removeNode(siblings, elseBinding.node);
-            }
-        } else if (elseBinding) {
-            transformElseBinding(path, ifBinding, elseBinding);
-            removeNode(siblings, elseBinding.node);
-        }
-
-        function getAttrNamesFromOption() {
-            const {
-                ifAttrName: optIfAttrName,
-                elseAttrName: optElseAttrName,
-                elseIfAttrName: optElseIfAttrName
-            } = this.opts || {};
-            return {
-                ifAttrName: optIfAttrName || 'if',
-                elseAttrName: optElseAttrName || 'else',
-                elseIfAttrName: optElseIfAttrName || 'elseIf',
-            }
-        }
+    if (!elseBinding) {
+      let elseIfBinding = getAttrASTAndIndexByName(nextNode, elseIfAttrName);
+      while (elseIfBinding) {
+        elseIfBindings.push(elseIfBinding);
+        index += 1;
+        nextNode = findNextNode(path, siblings, index);
+        elseIfBinding = nextNode ?
+          getAttrASTAndIndexByName(nextNode, elseIfAttrName) :
+          null;
+      }
+      if (nextNode) {
+        elseBinding = getAttrASTAndIndexByName(nextNode, elseAttrName);
+      }
     }
 
-    return {
-        visitor: {
-            JSXElement: JSXElementVisitor
-        }
+    if (elseIfBindings.length > 0) {
+      transformElseIfBindings(path, ifBinding, elseIfBindings, elseBinding);
+
+      elseIfBindings.forEach((binding) => {
+        removeNode(siblings, binding.node);
+      });
+      if (elseBinding) {
+        removeNode(siblings, elseBinding.node);
+      }
+    } else if (elseBinding) {
+      transformElseBinding(path, ifBinding, elseBinding);
+      removeNode(siblings, elseBinding.node);
     }
+  }
+
+  return {
+    visitor: {
+      JSXElement: JSXElementVisitor
+    }
+  }
 };
